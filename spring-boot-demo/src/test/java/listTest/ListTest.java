082fcfb77cfe4e84b3fcb2b59c1e54a18176b30e
zhaoshuxue <zhaoshuxue@163.com>
2017-02-26 19:17:32 +0800

    email code

---
+++ spring-boot-demo/src/test/java/listTest/ListTest.java
@@ -0,0 +1,21 @@
+package listTest;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		List<Integer> list = new ArrayList<Integer>();
+		
+		list.add(1);
+		list.add(2);
+		list.add(3);
+		list.add(5);
+		
+		
+		System.out.println(list);
+	}
+
+}
