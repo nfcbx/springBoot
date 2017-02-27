03856a3335727bed0e3bc1780effe95bd5881234
zhaoshuxue <zhaoshuxue@163.com>
2017-02-27 18:14:44 +0800

    'code'

---
+++ zhao.py
@@ -0,0 +1,25 @@
+import os
+
+os.system("git status")
+
+os.system("pause")
+
+os.system("git pull")
+
+os.system("pause")
+
+os.system("git add .")
+
+os.system("pause")
+
+os.system("git commit -m 'code'")
+
+os.system("pause")
+
+os.system("git push -u origin master")
+
+os.system("pause")
+
+
+#p=os.popen("git status")
+#print p.read()
