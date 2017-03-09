e6a918b02710174ee21c5493ccd3277943d7adfc
zhaoshuxue <zhaoshuxue@163.com>
2017-03-09 18:16:49 +0800

    code

--- spring-boot-demo/src/test/java/Sort/QuickSort.java
+++ spring-boot-demo/src/test/java/Sort/QuickSort.java
@@ -13,48 +13,51 @@ public class QuickSort {
 			arr[i] = random.nextInt(50);
 		}
 		System.out.print("排序前：");
-		for (int i : arr) {
-			System.out.print(i + " , ");
-		}
+		System.out.println(printArray(arr));
 		System.out.println();
 		
 		quickSort(arr, 0, arr.length - 1);
 		
 		System.out.print("排序后：");
-		for (int i : arr) {
-			System.out.print(i + " , ");
-		}
+		System.out.println(printArray(arr));
 		System.out.println();
 
 	}
+	
+	/**
+	 * 基本思想：
+	 * 选择一个基准元素,通常选择第一个元素或者最后一个元素,通过一趟扫描，
+	 * 将待排序列分成两部分,一部分比基准元素小,一部分大于等于基准元素,此时基准元素在其排好序后的正确位置,
+	 * 然后再用同样的方法递归地排序划分的两部分。
+	 * @param arr
+	 * @param low
+	 * @param high
+	 * @return
+	 */
+	private static int partition(int[] arr, int low, int high) {
 
-	private static int partition(int[] arr, int i, int j) {
+		int key = arr[low];// key值
+		while (low < high) {
 
-		int key = arr[i];// key值
-		while (i < j) {
-
-			while (i < j && arr[j] > key) {// 从右向左小循环
-				j--;
-				System.out.println(j);
+			while (low < high && arr[high] >= key) {// 从右向左小循环
+				high--;
 			}
-			if (arr[j] <= key) {// 判定填充
-				arr[i] = arr[j];
+			
+			arr[low] = arr[high]; // 比key值小的移到低端
+
+			while (low < high && arr[low] <= key) {// 从左向右小循环
+				low++;
 			}
 
-			while (i < j && arr[i] < key) {// 从左向右小循环
-				i++;
-				System.out.println(i);
-			}
+			arr[high] = arr[low]; // 比key值大的 移到高端
 
-			if (arr[i] >= key) {// 判定填充
-				arr[j] = arr[i];
-			}
-
+			System.out.println(printArray(arr));
+			System.out.println("low: " + low + "    high: " + high);
 		}
 
-		arr[i] = key;// 把轴元素放在轴所在地位置
+		arr[low] = key;// 把轴元素放在轴所在地位置
 
-		return i;// 返回轴所在的位置
+		return low;// 返回轴所在的位置
 	}
 
 	private static void quickSort(int data[], int low, int high) {
@@ -66,6 +69,15 @@ public class QuickSort {
 			quickSort(data, q + 1, high);// 对q左边进行分类
 			quickSort(data, low, q - 1);// 对q右边进行分类
 		}
+	}
+	
+	
+	public static String printArray(int[] arr) {
+		StringBuffer str = new StringBuffer();
+		for (int i : arr) {
+			str.append(i + " , ");
+		}
+		return str.toString();
 	}
 
 }
