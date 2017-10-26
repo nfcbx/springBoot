3218c051f6966d9b133a6025fc52c4c80e2994a3
zhaoshuxue <zhaoshuxue@163.com>
2017-10-26 18:54:44 +0800

    code

--- spring-boot-demo/src/main/java/com/zsx/App.java
+++ spring-boot-demo/src/main/java/com/zsx/App.java
@@ -1,10 +1,12 @@
 package com.zsx;
 
+import javax.swing.JFrame;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class App {
+public class App extends JFrame{
 
 	public static void main(String[] args) {
 		SpringApplication.run(App.class, args);
--- spring-boot-demo/src/main/java/com/zsx/controller/TestController.java
+++ spring-boot-demo/src/main/java/com/zsx/controller/TestController.java
@@ -3,9 +3,14 @@ package com.zsx.controller;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.alibaba.fastjson.JSONObject;
+import com.zsx.util.ExecutorUtil;
 
 @RestController
 public class TestController {
@@ -17,11 +22,39 @@ public class TestController {
 	
 	@GetMapping(value = {"/a", "/b", "/c"})
 	public String name2(HttpServletRequest request) {
-		
+		try {
+			Thread.sleep(10000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		return "Hello " + request.getServletPath();
 	}
 	
 	
+	@GetMapping(value = {"/aa"})
+	public Object name3(HttpServletRequest request, @RequestParam(value = "name", defaultValue="123") String name) {
+		JSONObject object = new JSONObject();
+		
+		
+		ExecutorUtil.executor.execute(new Runnable() {
+			
+			@Override
+			public void run() {
+				object.put("code", 200);
+				object.put("message", "阿斯蒂芬加拉设计费拉丝机枫蓝国际啦爱神的箭法拉时代峻峰两份礼物而");
+				object.put("data", "{\"list\":[{\"id\":80,\"email\":\"zhanglin@qiand.me\"}]}");
+				try {
+					Thread.sleep(2000);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+				
+			}
+		});
+		
+		return object;
+	}
 	
 	
 	
