603adf7ef1762f67b9a69330b309b2a5ff5386ab
zhaoshuxue <zhaoshuxue@163.com>
2018-10-26 19:24:06 +0800

    commitByZsx

---
+++ code/code-demo/src/test/java/lambda/Demo1026.java
@@ -0,0 +1,31 @@
+package lambda;
+
+import com.google.common.collect.Lists;
+
+import java.util.ArrayList;
+
+/**
+ * Created by ZSX on 2018/10/26.
+ *
+ * @author ZSX
+ */
+public class Demo1026 {
+
+    public static void main(String[] args) {
+        ArrayList<Integer> list = Lists.newArrayList(111, 222, 333, 444, 555);
+
+        System.out.println("以前的循环方式");
+        for (Integer integer : list) {
+            System.out.println(integer);
+        }
+
+        System.out.println("lambda循环方式");
+        list.forEach((object) -> System.out.println(object));
+
+        System.out.println("在 Java 8 中使用双冒号操作符");
+        list.forEach(System.out::println);
+
+
+    }
+
+}
