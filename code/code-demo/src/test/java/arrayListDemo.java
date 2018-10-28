7db596edce8c186b73ef6b3f91ee64cc30bbbdbb
zhaoshuxue <zhaoshuxue@163.com>
2018-10-28 18:03:29 +0800

    commitByZsx

---
+++ code/code-demo/src/test/java/arrayListDemo.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by highness on 2018/10/28 0028.
+ */
+public class arrayListDemo {
+
+    public static void main(String[] args) {
+        List list = new ArrayList();
+
+        list.add(1);
+
+
+    }
+
+}
