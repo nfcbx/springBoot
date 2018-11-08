d7f920168931cd59d898c8eaf926435e6685cee6
zhaoshuxue <zhaoshuxue@163.com>
2018-11-08 19:59:12 +0800

    commitByZsx

--- code/code-demo/src/test/java/newIODemo/NewIOTest1.java
+++ code/code-demo/src/test/java/newIODemo/NewIOTest1.java
@@ -1,5 +1,8 @@
 package newIODemo;
 
+import java.io.IOException;
+import java.nio.channels.Channel;
+
 /**
  * Created by ZSX on 2018/11/7.
  *
@@ -10,6 +13,17 @@ public class NewIOTest1 {
     public static void main(String[] args) {
 
 
+        Channel channel = new Channel() {
+            @Override
+            public boolean isOpen() {
+                return false;
+            }
+
+            @Override
+            public void close() throws IOException {
+
+            }
+        };
 
 
     }
