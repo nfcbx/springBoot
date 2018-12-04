acba402ca054c99939fbe80279e9f8199598bb81
zhaoshuxue <zhaoshuxue@163.com>
2018-12-04 16:13:09 +0800

    commitByZsx

--- example/springboot-log4j2/src/main/java/com/zsx/controller/TestController.java
+++ example/springboot-log4j2/src/main/java/com/zsx/controller/TestController.java
@@ -1,9 +1,29 @@
 package com.zsx.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+
 /**
  * Created by ZSX on 2018/12/4.
  *
  * @author ZSX
  */
+@RestController
 public class TestController {
+
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+
+    @GetMapping("/")
+    public String test() {
+
+        logger.info("打印日志：{}", new Date());
+
+        return "Hello World!!!";
+    }
+
 }
