7337058453e304efe0812f1a7496b79a84d56e2e
zhaoshuxue <zhaoshuxue@163.com>
2019-06-10 20:03:26 +0800

    commitByZsx

--- springboot2.0/springboot-ueditor/src/main/resources/db/schema.sql
+++ springboot2.0/springboot-ueditor/src/main/resources/db/schema.sql
@@ -1,4 +1,11 @@
-CREATE TABLE user (
+CREATE TABLE if not exists user (
 id INT,
 name VARCHAR
+);
+
+create table if not exists notebook (
+  id int primary key not null,
+  title VARCHAR(100) not null,
+  content VARCHAR(999999) null,
+  createtime TIMESTAMP not null
 );
