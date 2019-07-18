8a7fbded142b906542c248d1036481e5bdf6ed68
zhaoshuxue <zhaoshuxue@163.com>
2019-07-18 17:55:56 +0800

    2

--- code/code-demo/src/test/java/zipTest/ZipTest.java
+++ code/code-demo/src/test/java/zipTest/ZipTest.java
@@ -1,6 +1,11 @@
 package zipTest;
 
+import com.alibaba.fastjson.JSON;
+import com.google.common.collect.Lists;
+import org.joda.time.DateTime;
+
 import java.io.*;
+import java.util.ArrayList;
 import java.util.logging.Logger;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
@@ -17,8 +22,31 @@ public class ZipTest {
     private static final String CHAR_SET = "GBK";
 
     public static void main(String[] args) throws Exception {
-        test();
-        ;
+//        test();
+//        test2();
+
+        String rootPath = "D:/2/";
+        File file = new File(rootPath);
+        if (file.isDirectory()) {
+            File[] files = file.listFiles();
+            System.out.println(JSON.toJSONString(files));
+            String[] list = file.list();
+            System.out.println(JSON.toJSONString(list));
+            for (File file1 : files) {
+                System.out.println(file1.getName());
+            }
+        }
+
+        String yyyyMMddHHmm = DateTime.now().toString("-yyyy-MM-dd-HH-mm");
+        System.out.println(yyyyMMddHHmm);
+
+        File file1 = new File(rootPath, "22.txt");
+        System.out.println(file1.exists());
+
+
+        File file2 = new File(rootPath + "33.txt");
+        System.out.println(file2.exists());
+
     }
 
     public static void test() throws IOException {
@@ -54,9 +82,35 @@ public class ZipTest {
     }
 
 
-    public static void compressFile() {
+    public static void test2() throws IOException {
+        String rootPath = "D:/2/";
+        // 目标
+        ArrayList<String> files = Lists.newArrayList("1.txt", "2.txt", "3.txt");
+        // 结果文件
+        String zipFile = rootPath + "anjian.zip";
 
+        ZipOutputStream zipOutputStream = new ZipOutputStream(new FileOutputStream(new File(zipFile)));
+        BufferedInputStream bis = null;
+        for (String file : files) {
+            ZipEntry zipEntry = new ZipEntry(file);
+            zipOutputStream.putNextEntry(zipEntry);
 
+            bis = new BufferedInputStream(new FileInputStream(new File(rootPath + file)));
+
+            int count = 0;
+            byte[] bytes = new byte[BUFFER];
+            while ((count = bis.read(bytes, 0, BUFFER)) != -1) {
+                zipOutputStream.write(bytes, 0, count);
+            }
+        }
+
+        bis.close();
+        zipOutputStream.closeEntry();
+
+        // 冲刷输出流
+        zipOutputStream.flush();
+        // 关闭输出流
+        zipOutputStream.close();
     }
 
 
