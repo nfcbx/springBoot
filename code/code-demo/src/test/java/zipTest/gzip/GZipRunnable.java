65dc8b2bde2d8af9625d2b3e85ebe4182775be2d
zhaoshuxue <zhaoshuxue@163.com>
2019-07-18 17:55:48 +0800

    11

---
+++ code/code-demo/src/test/java/zipTest/gzip/GZipRunnable.java
@@ -0,0 +1,8 @@
+package zipTest.gzip;
+
+/**
+ * @author zhaoshuxue3
+ * @Date 2019/7/17 11:38
+ **/
+public class GZipRunnable {
+}
---
+++ code/code-demo/src/test/java/zipTest/gzip/GZip压缩测试.java
@@ -0,0 +1,12 @@
+package zipTest.gzip;
+
+/**
+ * @author zhaoshuxue3
+ * @Date 2019/7/17 11:37
+ **/
+public class GZip {
+
+
+
+
+}
