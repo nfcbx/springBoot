3b9a00a40f4b2c86bc4b0f72cee77caa147be20e
zhaoshuxue <zhaoshuxue@163.com>
2019-11-29 21:18:46 +0800

    commitByZsx

--- code/code-demo/src/test/java/多线程/CountDownLatchDemo2.java
+++ code/code-demo/src/test/java/多线程/CountDownLatchDemo2.java
@@ -1,4 +1,65 @@
 package 多线程;
 
+import org.joda.time.DateTime;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
 public class CountDownLatchDemo2 {
+
+
+    public static void main(String[] args) {
+        ExecutorService executorService = Executors.newFixedThreadPool(4);
+
+        print("串行开始时间:" + getTime());
+        for (int i = 0; i < 10; i++) {
+            int n = i;
+            runJob(n);
+        }
+        print("串行结束时间:" + getTime());
+
+
+        CountDownLatch countDownLatch = new CountDownLatch(6);
+
+        for (int i = 0; i < 10; i++) {
+            int n = i;
+            executorService.execute(() -> {
+                runJob(n);
+
+                countDownLatch.countDown();
+            });
+        }
+        print("打印位置  001");
+        try {
+            countDownLatch.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        print("打印位置  002");
+        executorService.shutdown();
+
+        print("打印位置  003");
+    }
+
+
+    private static void runJob(int i) {
+        try {
+            // 休眠，模拟job运行耗时
+            TimeUnit.SECONDS.sleep(2L);
+            print("运行任务i=" + i);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void print(String str) {
+        String time = getTime();
+        System.out.println(time + " : " + str);
+    }
+
+    private static String getTime() {
+        return DateTime.now().toString("yyyy-MM-dd HH:mm:ss");
+    }
 }
