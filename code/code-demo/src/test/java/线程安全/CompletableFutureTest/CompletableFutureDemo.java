b5f66dded12cd49901c059cad780104420f0e514
zhaoshuxue <zhaoshuxue@163.com>
2019-12-01 22:38:51 +0800

    commitByZsx

--- code/code-demo/src/test/java/线程安全/CompletableFutureTest/CompletableFutureDemo.java
+++ code/code-demo/src/test/java/线程安全/CompletableFutureTest/CompletableFutureDemo.java
@@ -1,8 +1,10 @@
 package 线程安全.CompletableFutureTest;
 
+import com.google.common.collect.Lists;
 import org.joda.time.LocalDateTime;
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.concurrent.*;
 
 public class CompletableFutureDemo {
@@ -38,7 +40,7 @@ public class CompletableFutureDemo {
     }
 
     public static String getNow() {
-        return LocalDateTime.now().toString("yyyy-MM-dd HH:mm:ss");
+        return LocalDateTime.now().toString("HH:mm:ss");
     }
 
 
@@ -58,8 +60,8 @@ public class CompletableFutureDemo {
         });
 
 
-
     }
+
     @Test
     public void test3() throws Exception {
 //        supplyAsync 获取返回结果
@@ -67,20 +69,101 @@ public class CompletableFutureDemo {
             try {
                 TimeUnit.SECONDS.sleep(5L);
             } catch (InterruptedException e) {
-               e.printStackTrace();
+                e.printStackTrace();
             }
             return "done";
         });
-        System.out.println("1");
+        System.out.println(getNow() + "  :  1");
         String str = future.get();
-        System.out.println("2");
+        System.out.println(getNow() + "    2");
         System.out.println(str);
     }
-    public void test4() throws Exception {}
-    public void test5() throws Exception {}
-    public void test6() throws Exception {}
-    public void test7() throws Exception {}
-    public void test8() throws Exception {}
-    public void test9() throws Exception {}
-    public void test10() throws Exception {}
+
+    @Test
+    public void test4() throws Exception {
+        CompletableFuture<String> future1 = CompletableFuture.supplyAsync(() -> {
+            try {
+                TimeUnit.SECONDS.sleep(5L);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return "第一个返回结果，需要5秒";
+        });
+
+        CompletableFuture<String> future2 = CompletableFuture.supplyAsync(() -> {
+            try {
+                TimeUnit.SECONDS.sleep(3L);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return "第二个返回结果，需要3秒";
+        });
+        System.out.println(getNow() + "  开始执行");
+
+        String str1 = future1.get();
+
+        System.out.println(getNow() + "  打印第一个结果 " + str1);
+
+        String str2 = future2.get();
+
+        System.out.println(getNow() + "  打印第一个结果 " + str2);
+
+//        22:25:22  开始执行
+//        22:25:27  打印第一个结果 第一个返回结果，需要5秒
+//        22:25:27  打印第一个结果 第二个返回结果，需要3秒
+    }
+
+    @Test
+    public void test5() throws Exception {
+        ArrayList<CompletableFuture<String>> resultList = Lists.newArrayList();
+        CompletableFuture<String> future = null;
+        for (int i = 0; i < 10; i++) {
+            int num = i;
+            future = CompletableFuture.supplyAsync(() -> {
+                System.out.println(getNow() + "开始执行job  " + num);
+                try {
+                    TimeUnit.SECONDS.sleep(2L);
+                } catch (InterruptedException e) {
+                }
+                return "返回 job " + num;
+            });
+            resultList.add(future);
+        }
+
+        System.out.println(getNow() + "  准备获取结果 ");
+
+//        if (future != null) {
+//            String res = future.get();
+//            System.out.println(getNow() + " 结果为：  " + res);
+//        }
+        System.out.println(getNow() + " 结果size：  " + resultList.size());
+        for (CompletableFuture<String> completableFuture : resultList) {
+            String res = completableFuture.get();
+            System.out.println(getNow() + "  结果为： " + res);
+        }
+
+        System.out.println(getNow() + "  全部结束 ");
+
+
+    }
+
+    @Test
+    public void test6() throws Exception {
+    }
+
+    @Test
+    public void test7() throws Exception {
+    }
+
+    @Test
+    public void test8() throws Exception {
+    }
+
+    @Test
+    public void test9() throws Exception {
+    }
+
+    @Test
+    public void test10() throws Exception {
+    }
 }
