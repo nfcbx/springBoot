7dd4a90419c72a42c30233a9d669e2832a0686f0
zhaoshuxue <zhaoshuxue@163.com>
2019-12-01 23:07:11 +0800

    commitByZsx

--- code/code-demo/src/test/java/多线程/CountDownLatchDemo2.java
+++ code/code-demo/src/test/java/多线程/CountDownLatchDemo2.java
@@ -1,11 +1,11 @@
 package 多线程;
 
+import com.google.common.collect.Lists;
 import org.joda.time.DateTime;
+import org.junit.Test;
 
-import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
+import java.util.ArrayList;
+import java.util.concurrent.*;
 
 public class CountDownLatchDemo2 {
 
@@ -65,5 +65,70 @@ public class CountDownLatchDemo2 {
 
     private static String getTime() {
         return DateTime.now().toString("yyyy-MM-dd HH:mm:ss");
+    }
+
+    private static String getLongTime(long start, long end) {
+        long l = end - start;
+        long second = l / 1000;
+        return "  耗时 " + second + "秒";
+    }
+
+    @Test
+    public void test1() {
+        long start = System.currentTimeMillis();
+        // 任务总数
+        int total = 10;
+
+        ThreadPoolExecutor threadPoolExecutor =
+                new ThreadPoolExecutor(4, 8, 5L, TimeUnit.SECONDS, new LinkedBlockingQueue<>());
+
+        ArrayList<CompletableFuture<String>> resultList = Lists.newArrayList();
+
+        CountDownLatch countDownLatch = new CountDownLatch(total);
+
+        print("开始执行任务");
+
+        for (int i = 0; i < total; i++) {
+            int num = i;
+            CompletableFuture<String> future = CompletableFuture.supplyAsync(() -> {
+                try {
+                    print("开始执行任务 ： " + num);
+                    TimeUnit.SECONDS.sleep(2L);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                countDownLatch.countDown();
+                return "任务结果" + num;
+            }, threadPoolExecutor);
+
+            resultList.add(future);
+        }
+
+        print("for循环结束");
+
+        try {
+            print("开始等待计数器为0");
+            countDownLatch.await();
+            print("计数器已经为0");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        print("执行完成，开始打印结果");
+        for (CompletableFuture<String> completableFuture : resultList) {
+            try {
+                String res = completableFuture.get();
+                print("结果为： " + res);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } catch (ExecutionException e) {
+                e.printStackTrace();
+            }
+        }
+        print("结果打印完毕");
+
+        print("任务全部执行结束");
+        long end = System.currentTimeMillis();
+        print(getLongTime(start, end));
     }
 }
