7558e99d7208b1e2c91d33b65ff280fbde4e6efe
zhaoshuxue <zhaoshuxue@163.com>
2019-12-22 19:32:44 +0800

    commitByZsx

--- code/code-demo/src/test/java/poiDemo/csv/CsvApacheDemo.java
+++ code/code-demo/src/test/java/poiDemo/csv/CsvApacheDemo.java
@@ -6,23 +6,25 @@ import org.apache.commons.csv.CSVPrinter;
 import org.junit.Test;
 
 import java.io.FileOutputStream;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.util.ArrayList;
 
 public class CsvApacheDemo {
 
+    public static final String charsetName = "GBK";
 
     @Test
     public void createCSVFile() throws IOException {
         ArrayList<String> list = Lists.newArrayList("中文", "中文", "中文", "中文");
 
 //        FileWriter writer = new FileWriter("D://3.csv");
-        OutputStreamWriter writer = new OutputStreamWriter(new FileOutputStream("D://3.csv", true), "UTF-8");
+        FileOutputStream fileOutputStream = new FileOutputStream("D://3.csv", true);
+        OutputStreamWriter writer = new OutputStreamWriter(fileOutputStream, "UTF-8");
 
         CSVPrinter printer = new CSVPrinter(writer, CSVFormat.DEFAULT);
         printer.printRecord(new String(new byte[]{(byte) 0xEF, (byte) 0xBB, (byte) 0xBF}));
+
         list.forEach(obj -> {
             try {
                 printer.printRecord(obj, obj, obj);
