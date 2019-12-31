d1ec9732d4d5d43b18f845923caf750c2954ee18
zhaoshuxue <zhaoshuxue@163.com>
2019-12-31 21:37:40 +0800

    commitByZsx

--- code/code-demo/src/main/java/com/zsx/utils/ThreadPoolUtil.java
+++ code/code-demo/src/main/java/com/zsx/utils/ThreadPoolUtil.java
@@ -1,6 +1,21 @@
 package com.zsx.utils;
 
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
 public class ThreadPoolUtil {
 
-    
+    private static ThreadPoolExecutor threadPoolExecutor = null;
+
+    private ThreadPoolUtil() {
+    }
+
+    public static ThreadPoolExecutor getInstance() {
+        if (threadPoolExecutor == null) {
+            threadPoolExecutor = new ThreadPoolExecutor(1, 2, 1L, TimeUnit.SECONDS, null);
+        }
+        return threadPoolExecutor;
+    }
+
+
 }
