fb98f0f78277dc2218df2bf24f85b610fd209a40
zhaoshuxue <zhaoshuxue@163.com>
2020-01-13 23:38:20 +0800

    commitByZsx

--- springboot2.0/springboot-mybatis-cache/src/test/java/com/zsx/AppTests.java
+++ springboot2.0/springboot-mybatis-cache/src/test/java/com/zsx/AppTests.java
@@ -1,5 +1,9 @@
 package com.zsx;
 
+import com.alibaba.fastjson.JSON;
+import com.zsx.dao.UserDao;
+import com.zsx.entity.User;
+import com.zsx.service.UserService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -7,32 +11,65 @@ import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
 
+import java.util.List;
 import java.util.Set;
 
 
 @SpringBootTest
 class AppTests {
 
-	@Autowired
-	RedisTemplate redisTemplate;
-	@Autowired
-	StringRedisTemplate stringRedisTemplate;
+    @Autowired
+    RedisTemplate redisTemplate;
+    @Autowired
+    StringRedisTemplate stringRedisTemplate;
 
-	@Test
-	void contextLoads() {
-		System.out.println(8899);
-		System.out.println(redisTemplate);
+    @Autowired
+    UserDao userDao;
+    @Autowired
+    private UserService userService;
 
-		ValueOperations<String, String> operations = stringRedisTemplate.opsForValue();
+    @Test
+    void test1() {
+        List<User> all = userService.getAll();
+        System.out.println(8899);
+        System.out.println(JSON.toJSONString(all));
+
+        System.out.println();
 
 
-		operations.set("name", "asdf");
+        List<User> all2 = userService.getAll();
+        System.out.println(8899);
+        System.out.println(JSON.toJSONString(all2));
+    }
 
-		Set keys = redisTemplate.keys("*");
-		for (Object key : keys) {
-			System.out.println(key);
-		}
-	}
+    @Test
+    void test2() {
+        List<User> users = userDao.selectAll();
+        System.out.println(8899);
+        System.out.println(JSON.toJSONString(users));
+
+        System.out.println();
+
+        List<User> users2 = userDao.selectAll();
+        System.out.println(8899);
+        System.out.println(JSON.toJSONString(users2));
+    }
+
+    @Test
+    void contextLoads() {
+        System.out.println(8899);
+        System.out.println(redisTemplate);
+
+        ValueOperations<String, String> operations = stringRedisTemplate.opsForValue();
+
+
+        operations.set("name", "asdf");
+
+        Set keys = redisTemplate.keys("*");
+        for (Object key : keys) {
+            System.out.println(key);
+        }
+    }
 
 
 }
