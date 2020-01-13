60e02c8658be159e1d568fd3a5615b91f08e2eba
zhaoshuxue <zhaoshuxue@163.com>
2020-01-13 20:38:01 +0800

    commitByZsx

--- code/code-demo/src/test/java/EsDemo/EsTest.java
+++ code/code-demo/src/test/java/EsDemo/EsTest.java
@@ -2,14 +2,14 @@ package EsDemo;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
-import com.google.common.collect.Maps;
 import com.zsx.config.EsSearchUtil;
+import jdk.management.resource.internal.inst.SocketOutputStreamRMHooks;
 import org.elasticsearch.action.admin.indices.create.CreateIndexResponse;
 import org.elasticsearch.action.admin.indices.mapping.put.PutMappingRequest;
 import org.elasticsearch.action.admin.indices.mapping.put.PutMappingResponse;
 import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.action.search.SearchRequestBuilder;
 import org.elasticsearch.action.search.SearchResponse;
-import org.elasticsearch.action.search.SearchType;
 import org.elasticsearch.action.support.WriteRequest;
 import org.elasticsearch.action.update.UpdateResponse;
 import org.elasticsearch.client.Client;
@@ -23,21 +23,27 @@ import org.elasticsearch.index.query.QueryBuilders;
 import org.elasticsearch.index.query.QueryStringQueryBuilder;
 import org.elasticsearch.rest.RestStatus;
 import org.elasticsearch.search.SearchHit;
+import org.elasticsearch.search.aggregations.Aggregation;
+import org.elasticsearch.search.aggregations.AggregationBuilder;
+import org.elasticsearch.search.aggregations.AggregationBuilders;
+import org.elasticsearch.search.aggregations.Aggregations;
+import org.elasticsearch.search.aggregations.bucket.filter.Filter;
+import org.elasticsearch.search.aggregations.metrics.valuecount.ValueCount;
 import org.junit.Before;
 import org.junit.Test;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.util.UUID;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
+import java.util.stream.IntStream;
 
 public class EsTest {
 
     private Client client = null;
 
-    private static String indexName = "test-1";
-    private static String indexType = "test";
+    private static String indexName = "demo2";
+    private static String indexType = "demo2";
 
     @Before
     public void before() {
@@ -112,6 +118,7 @@ public class EsTest {
         }
     }
 
+
     @Test
     public void test5() throws IOException, InterruptedException {
         int size = 10;
@@ -161,6 +168,106 @@ public class EsTest {
             RestStatus status = updateResponse.status();
             System.out.println("id= " + id + "     status= " + JSON.toJSONString(status));
         }
+
+    }
+
+    @Test
+    public void 创建索引() throws IOException {
+
+        String indexName = "demo2";
+        String indexType = "demo2";
+
+        client.admin().indices().prepareCreate(indexName).get();
+
+        XContentBuilder builder = null;
+        builder = XContentFactory.jsonBuilder()
+                .startObject()
+                .field("properties")
+                .startObject()
+
+                .field("name").startObject().field("type", "keyword").endObject()
+                .field("name1").startObject().field("type", "string").field("index", "not_analyzed").endObject()
+                .field("name2").startObject().field("type", "string").endObject()
+
+                .field("type").startObject().field("type", "string").endObject()
+
+                .field("age").startObject().field("type", "integer").field("index", "not_analyzed").endObject()
+                .field("create_time").startObject().field("type", "long").field("index", "not_analyzed").endObject()
+                .endObject()
+                .endObject();
+
+        PutMappingRequest mappingRequest = Requests.putMappingRequest(indexName).type(indexType).source(builder);
+        PutMappingResponse response = client.admin().indices().putMapping(mappingRequest).actionGet();
+        boolean acknowledged = response.isAcknowledged();
+        System.out.println(acknowledged);
+    }
+
+    @Test
+    public void 创建数据() throws IOException {
+        IntStream.range(30, 40).parallel().forEach((i) -> {
+            JSONObject jsonObject = new JSONObject();
+            jsonObject.put("name", "name" + i);
+            jsonObject.put("name1", "name" + i);
+            jsonObject.put("name2", "name" + i);
+            jsonObject.put("type", "type3");
+            jsonObject.put("age", i);
+            jsonObject.put("create_time", System.currentTimeMillis());
+            IndexResponse indexResponse = client.prepareIndex(indexName, indexType)
+                    .setSource(jsonObject.toJSONString(), XContentType.JSON)
+                    .get();
+            RestStatus status = indexResponse.status();
+            System.out.println(JSON.toJSONString(status));
+        });
+
+    }
+
+    @Test
+    public void 聚合数据() throws ExecutionException, InterruptedException {
+
+//        BoolQueryBuilder boolQueryBuilder = QueryBuilders.boolQuery().mustNot(QueryBuilders.matchQuery("type", "type1"));
+//        BoolQueryBuilder boolQueryBuilder = QueryBuilders.boolQuery().must(QueryBuilders.matchQuery("type", "type1"));
+
+        BoolQueryBuilder boolQueryBuilder = QueryBuilders.boolQuery()
+                .mustNot(QueryBuilders.matchQuery("type", "type1"))
+                .mustNot(QueryBuilders.matchQuery("type", "type2"));
+
+
+        AggregationBuilder aggregationBuilder = AggregationBuilders.filter("name_filter", boolQueryBuilder)
+                .subAggregation(AggregationBuilders.count("name_count").field("name"));
+
+        BoolQueryBuilder queryBuilder = QueryBuilders.boolQuery();
+
+        SearchRequestBuilder searchRequest = client.prepareSearch(indexName).setTypes(indexType).setQuery(queryBuilder);
+
+        searchRequest.addAggregation(aggregationBuilder);
+
+
+        SearchResponse searchResponse = client.search(searchRequest.request()).get();
+
+        System.out.println(8899);
+        System.out.println(JSON.toJSONString(searchResponse));
+        Aggregations aggregations = searchResponse.getAggregations();
+
+        System.out.println(JSON.toJSONString(aggregations));
+//
+//        Aggregation name = aggregations.get("name_filter");
+//        System.out.println(JSON.toJSONString(name));
+
+        Filter nameFilter = (Filter) aggregations.get("name_filter");
+        System.out.println(JSON.toJSONString(nameFilter));
+        System.out.println(JSON.toJSONString(nameFilter.getAggregations()));
+
+        Aggregation name_count = nameFilter.getAggregations().get("name_count");
+
+        System.out.println(JSON.toJSONString(name_count));
+
+        ValueCount nameCount = (ValueCount) name_count;
+        System.out.println(JSON.toJSONString(nameCount));
+
+        long value = nameCount.getValue();
+
+        System.out.println(value);
+
 
     }
 
--- springboot2.0/springboot-mybatis-cache/src/test/java/com/zsx/AppTests.java
+++ springboot2.0/springboot-mybatis-cache/src/test/java/com/zsx/AppTests.java
@@ -1,13 +1,38 @@
 package com.zsx;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+
+import java.util.Set;
+
 
 @SpringBootTest
 class AppTests {
 
+	@Autowired
+	RedisTemplate redisTemplate;
+	@Autowired
+	StringRedisTemplate stringRedisTemplate;
+
 	@Test
 	void contextLoads() {
+		System.out.println(8899);
+		System.out.println(redisTemplate);
+
+		ValueOperations<String, String> operations = stringRedisTemplate.opsForValue();
+
+
+		operations.set("name", "asdf");
+
+		Set keys = redisTemplate.keys("*");
+		for (Object key : keys) {
+			System.out.println(key);
+		}
 	}
+
 
 }
