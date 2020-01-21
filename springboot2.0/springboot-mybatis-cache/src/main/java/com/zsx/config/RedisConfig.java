51be2d028e0d108a83fc9b578a70020556fffea3
zhaoshuxue <zhaoshuxue@163.com>
2020-01-21 22:15:07 +0800

    commitByZsx

--- springboot2.0/springboot-mybatis-cache/src/main/java/com/zsx/config/RedisConfig.java
+++ springboot2.0/springboot-mybatis-cache/src/main/java/com/zsx/config/RedisConfig.java
@@ -2,6 +2,7 @@ package com.zsx.config;
 
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
