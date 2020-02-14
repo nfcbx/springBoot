6b2cf23b10708fdb8a621250f8e8abfd7202cdec
zhaoshuxue <zhaoshuxue@163.com>
2020-02-14 20:10:37 +0800

    commitByZsx

--- code/code-demo/src/test/java/nettyTest/NettyEncoder.java
+++ code/code-demo/src/test/java/nettyTest/NettyEncoder.java
@@ -1,6 +1,14 @@
 package nettyTest;
 
-public class NettyEncoder {
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.codec.MessageToByteEncoder;
 
-    
+public class NettyEncoder extends MessageToByteEncoder<Object> {
+
+
+    @Override
+    protected void encode(ChannelHandlerContext context, Object in, ByteBuf out) throws Exception {
+        out.writeBytes(new byte[]{});
+    }
 }
