e3b82dc982b4f4a5240b73f15b4d533f614b3fe1
zhaoshuxue <zhaoshuxue@163.com>
2020-02-28 20:27:00 +0800

    commitByZsx

--- code/code-demo/src/test/java/工具/JerseyDemo.java
+++ code/code-demo/src/test/java/工具/JerseyDemo.java
@@ -1,7 +1,30 @@
 package 工具;
 
+import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.ClientResponse;
+import com.sun.jersey.api.client.WebResource;
+import org.junit.Test;
+
+import javax.ws.rs.core.MediaType;
+
 public class JerseyDemo {
 
 
+    @Test
+    public void test1(){
+
+
+        Client client = Client.create();
+
+        WebResource resource = client.resource("http://www.baidu.com");
+
+        ClientResponse response = resource.type(MediaType.APPLICATION_JSON_TYPE)
+                .get(ClientResponse.class);
+
+        String responseEntity = response.getEntity(String.class);
+
+        System.out.println(responseEntity);
+
+    }
 
 }
