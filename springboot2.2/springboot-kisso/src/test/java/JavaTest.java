4218be063112ed169e001488036d1152f4948c43
zhaoshuxue <zhaoshuxue@163.com>
2020-05-06 14:58:16 +0800

    :bug:push

--- springboot2.2/springboot-kisso/src/test/java/JavaTest.java
+++ springboot2.2/springboot-kisso/src/test/java/JavaTest.java
@@ -1,6 +1,8 @@
 import com.baomidou.kisso.SSOHelper;
 import org.junit.jupiter.api.Test;
 
+import javax.sound.midi.Soundbank;
+
 public class JavaTest {
 
 
@@ -8,6 +10,18 @@ public class JavaTest {
     public void test1() {
         String secretKey = SSOHelper.getHS512SecretKey();
         System.out.println(secretKey);
+    }
+
+    @Test
+    public void test2(){
+//        if(((double)Math.abs((user.hashCode())%1000)/1000)>identitySdkMarkRatio){
+
+
+        double v = ((double) Math.abs("树学网店".hashCode()) % 1000) / 1000;
+        System.out.println(v);
+
+        v = ((double) Math.abs("猫太郎么么".hashCode()) % 1000) / 1000;
+        System.out.println(v);
     }
 
 }
