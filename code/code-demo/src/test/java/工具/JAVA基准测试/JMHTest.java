6b847fc3984070479cd0862830ac10341d47e203
zhaoshuxue <zhaoshuxue@163.com>
2020-05-10 23:09:13 +0800

    commitByZsx

--- code/code-demo/src/test/java/工具/JAVA基准测试/JMHTest.java
+++ code/code-demo/src/test/java/工具/JAVA基准测试/JMHTest.java
@@ -9,6 +9,20 @@ import org.openjdk.jmh.runner.options.OptionsBuilder;
 
 import java.util.UUID;
 
+/**
+ *
+ * warmupIterations	预热的迭代次数	指定预热的迭代次数	@Warmup
+ * warmupBatchSize	预热批量的大小	指定预热批量的大小	@Warmup
+ * warmupForks	预热模式：INDI，BULK，BULK_INDI	指定预热模式	@Warmup
+ * warmupMode	预热的模式	指定预热的模式	@Warmup
+ * warmupTime	预热的时间	指定预热的时间	@Warmup
+ * measurementIterations	测试的迭代次数	指定测试的迭代次数	@Measurement
+ * measurementBatchSize	测试批量的大小	指定测试批量的大小	@Measurement
+ * measurementTime	测试的时间	指定测试的时间	@Measurement
+ * mode	测试模式： Throughput（吞吐量）， AverageTime（平均时间），SampleTime（在测试中，随机进行采样执行的时间），SingleShotTime（在每次执行中计算耗时），All	指定测试的模式	@BenchmarkMode
+ *
+ *
+ */
 public class JMHTest {
 
 
