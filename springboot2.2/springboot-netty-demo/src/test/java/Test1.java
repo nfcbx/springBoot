916f3ee510bca3cb1c2f9bd5f48f60621e20073b
zhaoshuxue <zhaoshuxue@163.com>
2020-06-06 23:29:46 +0800

    commitByZsx

--- springboot2.2/springboot-netty-demo/src/test/java/Test1.java
+++ springboot2.2/springboot-netty-demo/src/test/java/Test1.java
@@ -1,6 +1,22 @@
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import org.junit.jupiter.api.Test;
+
 public class Test1 {
 
 
+    @Test
+    public void t(){
+        EventLoopGroup group = new NioEventLoopGroup();
+
+        Bootstrap bootstrap = new Bootstrap();
+
+        bootstrap.group(group)
+                .channel(NioSocketChannel.class)
+        ;
+    }
 
 
 }
