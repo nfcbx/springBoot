732d7a07b470b6dfed9b7b0e117109d3ea8390f3
zhaoshuxue <zhaoshuxue@163.com>
2020-06-10 00:17:21 +0800

    commitByZsx

--- springboot2.2/springboot-netty-demo/src/test/java/demo1/MyServer.java
+++ springboot2.2/springboot-netty-demo/src/test/java/demo1/MyServer.java
@@ -2,6 +2,7 @@ package demo1;
 
 import io.netty.bootstrap.ServerBootstrap;
 import io.netty.channel.ChannelFuture;
+import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
 
@@ -9,8 +10,8 @@ public class MyServer {
 
 
     public static void main(String[] args) {
-        NioEventLoopGroup bossGroup = new NioEventLoopGroup();
-        NioEventLoopGroup workerGroup = new NioEventLoopGroup();
+        EventLoopGroup bossGroup = new NioEventLoopGroup();
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
         try {
             ServerBootstrap serverBootstrap = new ServerBootstrap();
             serverBootstrap.group(bossGroup, workerGroup)
