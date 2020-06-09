f4d8efaf3c0e55634a8bce923b7fff64cd022792
zhaoshuxue <zhaoshuxue@163.com>
2020-06-09 23:27:00 +0800

    commitByZsx

--- springboot2.2/springboot-netty-demo/src/test/java/demo1/MyServerHandler.java
+++ springboot2.2/springboot-netty-demo/src/test/java/demo1/MyServerHandler.java
@@ -1,4 +1,17 @@
 package demo1;
 
-public class MyServerHandler {
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+
+public class MyServerHandler extends SimpleChannelInboundHandler<String> {
+    @Override
+    protected void channelRead0(ChannelHandlerContext channelHandlerContext, String s) throws Exception {
+
+
+
+
+
+
+
+    }
 }
