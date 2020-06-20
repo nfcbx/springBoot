da741732b92e56b4f929e9e28b462ff797b6835d
zhaoshuxue <zhaoshuxue@163.com>
2020-06-20 23:28:43 +0800

    commitByZsx

--- springboot2.2/springboot-netty-demo/src/test/java/demo2/MyChatServerInitializer.java
+++ springboot2.2/springboot-netty-demo/src/test/java/demo2/MyChatServerInitializer.java
@@ -5,6 +5,9 @@ import io.netty.channel.ChannelPipeline;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.handler.codec.DelimiterBasedFrameDecoder;
 import io.netty.handler.codec.Delimiters;
+import io.netty.handler.codec.string.StringDecoder;
+import io.netty.handler.codec.string.StringEncoder;
+import io.netty.util.CharsetUtil;
 
 public class MyChatServerInitializer extends ChannelInitializer<SocketChannel> {
     @Override
@@ -12,6 +15,8 @@ public class MyChatServerInitializer extends ChannelInitializer<SocketChannel> {
         ChannelPipeline pipeline = ch.pipeline();
 
         pipeline.addLast(new DelimiterBasedFrameDecoder(4096, Delimiters.lineDelimiter()));
-
+        pipeline.addLast(new StringDecoder(CharsetUtil.UTF_8));
+        pipeline.addLast(new StringEncoder(CharsetUtil.UTF_8));
+        pipeline.addLast(null);
     }
 }
