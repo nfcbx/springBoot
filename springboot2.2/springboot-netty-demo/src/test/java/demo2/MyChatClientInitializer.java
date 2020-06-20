dcd50cb8c0556a18bbfbeb0df0d778e716c798a0
zhaoshuxue <zhaoshuxue@163.com>
2020-06-20 23:57:02 +0800

    commitByZsx

--- springboot2.2/springboot-netty-demo/src/test/java/demo2/MyChatClientInitializer.java
+++ springboot2.2/springboot-netty-demo/src/test/java/demo2/MyChatClientInitializer.java
@@ -17,7 +17,7 @@ public class MyChatClientInitializer extends ChannelInitializer<SocketChannel> {
         pipeline.addLast(new DelimiterBasedFrameDecoder(4096, Delimiters.lineDelimiter()));
         pipeline.addLast(new StringDecoder(CharsetUtil.UTF_8));
         pipeline.addLast(new StringEncoder(CharsetUtil.UTF_8));
-        pipeline.addLast(null);
+        pipeline.addLast(new MyChatClientHandler());
 
     }
 }
