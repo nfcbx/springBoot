aa4bf3a2d55f25d9baa9d4c725166e17f6c7ab94
zhaoshuxue <zhaoshuxue@163.com>
2020-06-20 23:38:37 +0800

    commitByZsx

--- springboot2.2/springboot-netty-demo/src/test/java/demo2/MyChatServerHandler.java
+++ springboot2.2/springboot-netty-demo/src/test/java/demo2/MyChatServerHandler.java
@@ -1,12 +1,27 @@
 package demo2;
 
+import io.netty.channel.Channel;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.channel.group.ChannelGroup;
+import io.netty.channel.group.DefaultChannelGroup;
+import io.netty.util.concurrent.GlobalEventExecutor;
 
 public class MyChatServerHandler extends SimpleChannelInboundHandler<String> {
+
+    private static ChannelGroup channels = new DefaultChannelGroup(GlobalEventExecutor.INSTANCE);
+
     @Override
     protected void channelRead0(ChannelHandlerContext channelHandlerContext, String s) throws Exception {
 
+
+    }
+
+    // 连接建立
+    @Override
+    public void handlerAdded(ChannelHandlerContext ctx) throws Exception {
+        Channel channel = ctx.channel();
+        channel
 
     }
 
