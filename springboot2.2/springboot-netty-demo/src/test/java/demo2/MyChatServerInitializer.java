a771035539031aed07851ed0e20898c99c698c79
zhaoshuxue <zhaoshuxue@163.com>
2020-06-20 23:26:23 +0800

    commitByZsx

---
+++ springboot2.2/springboot-netty-demo/src/test/java/demo2/MyChatServerInitializer.java
@@ -0,0 +1,17 @@
+package demo2;
+
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.handler.codec.DelimiterBasedFrameDecoder;
+import io.netty.handler.codec.Delimiters;
+
+public class MyChatServerInitializer extends ChannelInitializer<SocketChannel> {
+    @Override
+    protected void initChannel(SocketChannel ch) throws Exception {
+        ChannelPipeline pipeline = ch.pipeline();
+
+        pipeline.addLast(new DelimiterBasedFrameDecoder(4096, Delimiters.lineDelimiter()));
+
+    }
+}
