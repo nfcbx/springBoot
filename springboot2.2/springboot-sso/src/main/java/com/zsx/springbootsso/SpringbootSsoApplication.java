f101001f63f71ec674e3fa5448b70e7fca39a923
zhaoshuxue <zhaoshuxue@163.com>
2020-06-23 23:47:54 +0800

    commitByZsx

---
+++ springboot2.2/springboot-sso/src/main/java/com/zsx/springbootsso/SpringbootSsoApplication.java
@@ -0,0 +1,13 @@
+package com.zsx.springbootsso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootSsoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootSsoApplication.class, args);
+    }
+
+}
---
+++ springboot2.2/springboot-sso/src/test/java/com/zsx/springbootsso/SpringbootSsoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zsx.springbootsso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootSsoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
