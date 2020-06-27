504a881583540fbe0e5a175add4dde200e447d03
zhaoshuxue <zhaoshuxue@163.com>
2020-06-28 00:59:51 +0800

    commitByZsx

--- springboot2.2/springboot-sso/sso-api/src/main/java/com/zsx/sso/SsoUser.java
+++ springboot2.2/springboot-sso/sso-api/src/main/java/com/zsx/sso/SsoUser.java
@@ -4,6 +4,38 @@ import java.io.Serializable;
 
 public class SsoUser implements Serializable {
 
+    private Integer userId;
+
+    private String username;
+
+    private String uuid;
 
 
+
+//  getter  setter
+
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(String uuid) {
+        this.uuid = uuid;
+    }
 }
