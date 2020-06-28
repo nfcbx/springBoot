2f18d4ecf2edebd485236a23c2362c03c501dd80
zhaoshuxue <zhaoshuxue@163.com>
2020-06-29 00:36:04 +0800

    commitByZsx

--- springboot2.2/springboot-sso/sso-api/src/main/java/com/zsx/sso/filter/SsoWebFilter.java
+++ springboot2.2/springboot-sso/sso-api/src/main/java/com/zsx/sso/filter/SsoWebFilter.java
@@ -1,10 +1,14 @@
 package com.zsx.sso.filter;
 
+import com.zsx.sso.SsoUser;
+import com.zsx.sso.helper.SsoLoginHelper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.servlet.*;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 public class SsoWebFilter extends HttpServlet implements Filter {
@@ -14,13 +18,30 @@ public class SsoWebFilter extends HttpServlet implements Filter {
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
         logger.info("SsoWebFilter init");
+//        filterConfig.getInitParameter("")
     }
 
     @Override
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain chain) throws IOException, ServletException {
         logger.info("SsoWebFilter doFilter");
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
 
-        chain.doFilter(request, response);
+        // make url
+        String servletPath = request.getServletPath();
+
+
+        SsoUser ssoUser = SsoLoginHelper.loginCheck(request, response);
+
+        if (ssoUser == null) {
+            // response
+            response.setStatus(HttpServletResponse.SC_OK);
+            response.setContentType("application/json;charset=UTF-8");
+            response.getWriter().println("{\"code\":501, \"msg\":\"请登录\"}");
+            return;
+        }
+
+        chain.doFilter(servletRequest, servletResponse);
         return;
     }
 }
---
+++ springboot2.2/springboot-sso/sso-server/src/main/java/com/zsx/sso/entity/TUser.java
@@ -0,0 +1,4 @@
+package com.zsx.sso.entity;
+
+public class TUser {
+}
